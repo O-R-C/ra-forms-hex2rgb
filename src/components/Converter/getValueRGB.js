9eb6b73de8c123c42a866462bbba616e9d923939
Andrey Semenov <semenov.orc@gmail.com>
2024-08-06 22:08:06 +0300

    add paintBackground

--- src/components/Converter/getValueRGB.js
+++ src/components/Converter/getValueRGB.js
@@ -1,3 +1,5 @@
+import paintBackground from './paintBackground'
+
 const convert2RGB = (value) => {
   const [r, g, b] = value.substr(1).match(/.{2}/g)
 
@@ -5,7 +7,13 @@ const convert2RGB = (value) => {
 }
 
 const getValueRGB = (value) => {
-  return value.length === 7 ? convert2RGB(value) : null
+  if (value.length !== 7) {
+    paintBackground()
+    return null
+  }
+
+  paintBackground(value)
+  return convert2RGB(value)
 }
 
 export default getValueRGB
---
+++ src/components/Converter/paintBackground.js
@@ -0,0 +1,5 @@
+const paintBackground = (value) => {
+  document.body.style.backgroundColor = value || '#fff'
+}
+
+export default paintBackground
