c7c0bcc03e255924fd1d414d08ce457abe157eab
Andrey Semenov <semenov.orc@gmail.com>
2024-08-06 21:25:45 +0300

    show RGB

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -5,6 +5,7 @@ import styles from './Converter.module.css'
 
 const Converter = () => {
   const [value, setValue] = useState('#')
+  const [valueRGB, setValueRGB] = useState(null)
 
   const onInputChange = ({ target: { value } }) => {
     if (value[0] !== '#') return
@@ -12,6 +13,12 @@ const Converter = () => {
     if (value.substr(1).match(/[^0-9a-f]/gi)) return
 
     setValue(value)
+
+    if (value.length === 7) {
+      const [r, g, b] = value.substr(1).match(/.{2}/g)
+
+      setValueRGB(`rgb(${Number.parseInt(r, 16)}, ${Number.parseInt(g, 16)}, ${Number.parseInt(b, 16)})`)
+    }
   }
 
   return (
@@ -20,7 +27,7 @@ const Converter = () => {
         value={value}
         onInputChange={onInputChange}
       />
-      <ResultRGB />
+      <ResultRGB valueRGB={valueRGB} />
     </div>
   )
 }
--- src/components/ResultRGB/ResultRGB.jsx
+++ src/components/ResultRGB/ResultRGB.jsx
@@ -1,11 +1,12 @@
+import propTypes from 'prop-types'
 import styles from './ResultRGB.module.css'
 
-const ResultRGB = () => {
-  return (
-    <div>
-      <h1>ResultRGB</h1>
-    </div>
-  )
+const ResultRGB = ({ valueRGB }) => {
+  return <div className={styles.result}>{valueRGB}</div>
+}
+
+ResultRGB.propTypes = {
+  valueRGB: propTypes.string,
 }
 
 export default ResultRGB
